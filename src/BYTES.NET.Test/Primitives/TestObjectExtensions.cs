7068f32b1bfe900301d23580930d67a1794838a7
Sebastian Räse <sebastian.raese@hauptstadtbytes.de>
2022-03-23 09:33:26 +0100

    BYTESDN-1:object conversion extension added; 'BYTES.NET' version set to '2.0.0.-alpha3'

---
+++ src/BYTES.NET.Test/Primitives/TestObjectExtensions.cs
@@ -0,0 +1,53 @@
+﻿//import .net namespace(s) required
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+//import .net namespace(s) required
+using BYTES.NET.Primitives.Extensions;
+
+namespace BYTES.NET.Test.Primitives
+{
+    [TestClass]
+    public class TestObjectExtensions
+    {
+        [TestMethod]
+        public void TestConversions()
+        {
+            //test string > bool conversions
+            object? input = "true";
+            
+            bool boolOutput;
+            Assert.AreEqual(true, input.TryConvert<bool>(out boolOutput));
+            Assert.AreEqual(true, boolOutput);
+
+            input = "Dummy";
+            Assert.AreEqual(false, input.TryConvert<bool>(out boolOutput));
+
+            //test string > integer conversions
+            input = "12";
+
+            int intOutput;
+            Assert.AreEqual(true, input.TryConvert<int>(out intOutput));
+            Assert.AreEqual(12, intOutput);
+
+            input = "Dummy";
+            Assert.AreEqual(false, input.TryConvert<int>(out intOutput));
+
+            //test custom conversions
+            Assert.AreEqual(true, input.TryConvert<int>(out intOutput, (object item) => {
+                                                                                            if (item.GetType() == typeof(string)) {
+                                                                                                return item.ToString().Length;
+                                                                                            } else
+                                                                                            {
+                                                                                                return default;
+                                                                                            }
+                                                                                        }));
+            Assert.AreEqual(5, intOutput);
+        }
+
+    }
+}
---
+++ src/BYTES.NET/Primitives/Extensions/Object.cs
@@ -0,0 +1,46 @@
+﻿//import .net namespace(s) required
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BYTES.NET.Primitives.Extensions
+{
+    public static class Object
+    {
+        #region public method(s)
+
+        public static bool TryConvert<T>(this object? input, out T? output, Func<object,T>? callback = null)
+        {
+            try
+            {
+                if(callback != null)
+                {
+                    output = callback(input);
+                } else
+                {
+                    Type underlyingType = Nullable.GetUnderlyingType(typeof(T));
+
+                    if (underlyingType != null && input == null)
+                    {
+                        output = default;
+                    } else
+                    {
+                        Type basetype = underlyingType == null ? typeof(T) : underlyingType;
+                        output = (T)Convert.ChangeType(input, basetype);
+                    }
+                }
+
+                return true;
+                
+            }catch(Exception ex)
+            {
+                output = default;
+                return false;
+            }
+        }
+
+        #endregion
+    }
+}
