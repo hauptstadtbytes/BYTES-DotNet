1b67d9504d80dee782cad48ad29f2696f04ccb92
Sebastian Räse <sebastian.raese@hauptstadtbytes.de>
2024-06-16 14:33:34 +0200

    BYTESNET-12: 'BYTES.NET.Automation' package added in version '1.0.0.0-alpha.1'

---
+++ src-2023/BYTES.NET.Automation/Scripting/Script.cs
@@ -0,0 +1,17 @@
+﻿//import .NET namespace(s) required
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//import namespace(s) requried from 'BYTES.NET' framework
+using BYTES.NET.Persistance.API;
+;
+
+namespace BYTES.NET.Automation.Scripting
+{
+    public class Script
+    {
+    }
+}
