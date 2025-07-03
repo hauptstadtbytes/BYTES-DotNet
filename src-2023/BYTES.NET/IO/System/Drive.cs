ab4bfe925f2c990dfdb5351efa71699f804303af
Aris411 <markushaubfleisch@outlook.com>
2025-07-03 12:52:29 +0200

    BYTESNET-31: Migrated the Classes and fixed some errors

---
+++ src-2023/BYTES.NET/IO/System/Drive.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace IO.System
+{
+    public class Drive
+    {
+        #region WinAPI
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern bool GetDiskFreeSpaceEx(
+            string lpDirectoryName,
+            out ulong lpFreeBytesAvailable,
+            out ulong lpTotalNumberOfBytes,
+            out ulong lpTotalNumberOfFreeBytes
+        );
+
+        #endregion
+
+        #region Private Fields
+
+        private readonly DriveInfo _drive;
+        private readonly bool _isRemovable;
+        private readonly ulong _totalSpace;
+        private readonly ulong _freeSpace;
+
+        #endregion
+
+        #region Public Properties
+
+        public DriveType Type => _drive.DriveType;
+
+        public bool IsRemovable => _isRemovable;
+
+        public string Path => _drive.Name;
+
+        public bool IsReady => _drive.IsReady;
+
+        /// <summary>
+        /// Returns total disk space in specified unit (default: GB).
+        /// </summary>
+        public double TotalSpace(string displayUnit = "GB", bool fullUnitsOnly = false)
+        {
+            if (!IsReady)
+                return 0;
+
+            return Formatter.FormatMemory(_totalSpace, displayUnit, fullUnitsOnly);
+        }
+
+        /// <summary>
+        /// Returns free disk space in specified unit (default: GB).
+        /// </summary>
+        public double FreeSpace(string displayUnit = "GB", bool fullUnitsOnly = false)
+        {
+            if (!IsReady)
+                return 0;
+
+            return Formatter.FormatMemory(_freeSpace, displayUnit, fullUnitsOnly);
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Create a Drive instance from a DriveInfo object.
+        /// </summary>
+        public Drive(DriveInfo drive)
+        {
+            _drive = drive;
+            _isRemovable = CheckIfRemovable(drive);
+            (_freeSpace, _totalSpace) = GetDriveSpace(drive.Name);
+        }
+
+        /// <summary>
+        /// Create a Drive instance using a drive letter (e.g., "C").
+        /// </summary>
+        public Drive(string letter) : this(new DriveInfo(letter))
+        {
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private bool CheckIfRemovable(DriveInfo drive)
+        {
+            return drive.DriveType == DriveType.Removable || drive.DriveType == DriveType.CDRom;
+        }
+
+        private (ulong free, ulong total) GetDriveSpace(string folderName)
+        {
+            if (string.IsNullOrWhiteSpace(folderName))
+                return (0, 0);
+
+            if (!folderName.EndsWith("\\"))
+                folderName += "\\";
+
+            if (GetDiskFreeSpaceEx(folderName, out ulong freeBytes, out ulong totalBytes, out _))
+                return (freeBytes, totalBytes);
+
+            return (0, 0);
+        }
+
+        #endregion
+    }
+}
---
+++ src-2023/BYTES.NET/IO/System/Info.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Net.NetworkInformation;
+using BYTES.NET.IO;
+
+namespace IO.System
+{
+    public class Info
+    {
+        #region public properties
+
+        public string Name
+        {
+            get { return Dns.GetHostName(); }
+        }
+        public string Domain
+        {
+            get
+            {
+                var localIPProperties = IPGlobalProperties.GetIPGlobalProperties();
+                return localIPProperties.DomainName;
+            }
+        }
+
+        public double Memory(string displayUnit = "GB", bool fullUnitsOnly = false)
+        {
+            //var memoryInfo = GC.GetGCMemoryInfo();
+            //return Helper.FormatMemory((ulong)memoryInfo.TotalAvailableMemoryBytes, displayUnit, fullUnitsOnly);
+
+            ObjectQuery query = new ObjectQuery("SELECT TotalPhysicalMemory FROM Win32_ComputerSystem");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ulong totalMemory = 0;
+
+            foreach (ManagementObject mo in searcher.Get())
+            {
+                totalMemory = (ulong)mo["TotalPhysicalMemory"];
+            }
+            return Formatter.FormatMemory(totalMemory, displayUnit, fullUnitsOnly);
+
+        }
+
+        public int Processors
+        {
+            get
+            {
+                return Environment.ProcessorCount;
+            }
+        }
+
+
+        public Dictionary<NetworkInterfaceType, List<Adapter>> Adapters
+        {
+            get { return GetClusteredAdapters(); }
+        }
+        public Adapter[] GetAdapters(NetworkInterfaceType adapterType) =>
+            Adapters.TryGetValue(adapterType, out var adapters) ? adapters.ToArray() : Array.Empty<Adapter>();
+
+
+        public User.Info CurrentUser
+        {
+            get
+            {
+                string userName = Environment.UserName;
+                string userDomain = Environment.UserDomainName;
+                return new User.Info(userName, null, userDomain);
+            }
+        }
+
+        public Dictionary<DriveType, List<Drive>> Drives
+        {
+            get { return GetClusteredDrives(); }
+        }
+
+        public Drive[] GetDrives(DriveType driveType) =>
+            Drives.TryGetValue(driveType, out var drives) ? drives.ToArray() : Array.Empty<Drive>();
+
+        #endregion
+
+        #region private methods
+
+        private Dictionary<NetworkInterfaceType, List<Adapter>> GetClusteredAdapters()
+        {
+            var output = new Dictionary<NetworkInterfaceType, List<Adapter>>();
+            foreach (var adapter in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (!output.TryGetValue(adapter.NetworkInterfaceType, out var adapterList))
+                {
+                    adapterList = new List<Adapter>();
+                    output[adapter.NetworkInterfaceType] = adapterList;
+                }
+                adapterList.Add(new Adapter(adapter));
+            }
+            return output;
+        }
+
+        private Dictionary<DriveType, List<Drive>> GetClusteredDrives()
+        {
+            var output = new Dictionary<DriveType, List<Drive>>();
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                if (!output.TryGetValue(drive.DriveType, out var driveList))
+                {
+                    driveList = new List<Drive>();
+                    output[drive.DriveType] = driveList;
+                }
+                driveList.Add(new Drive(drive));
+            }
+            return output;
+        }
+        #endregion
+    }
+}
