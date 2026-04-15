22d0860ae56045f28712246a962fe92c5022daa7
Valtiel Zaremba <valtiel.zaremba@hauptstadtbytes.de>
2026-04-15 16:58:47 +0200

    BYTESNET-48: Created MemoryInfo, deleted Formatter

---
+++ src-2023/BYTES.NET (Core)/BYTES.NET/IO/System/MemoryInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BYTES.NET.IO.System
+{
+    /// <summary>
+    /// Class to format Memory 
+    /// </summary>
+    public class MemoryInfo
+    {
+        #region private fields
+        private readonly ulong _bytes;
+        #endregion
+
+        #region constructor
+        public MemoryInfo(ulong bytes)
+        {
+            _bytes = bytes;
+        }
+        #endregion
+
+        #region public fields
+        public double InBytes => _bytes;
+        public double InKB => _bytes / 1024.0;
+        public double InMB => _bytes / 1024.0 / 1024.0;
+        public double InGB => _bytes / 1024.0 / 1024.0 / 1024.0;
+        public double InTB => _bytes / 1024.0 / 1024.0 / 1024.0 / 1024.0;
+        #endregion
+    }
+}
