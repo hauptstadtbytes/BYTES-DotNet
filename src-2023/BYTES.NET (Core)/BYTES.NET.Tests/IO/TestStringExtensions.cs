61ab3dd9aa7de0a567818dc48e68c9c5005c405f
Valtiel Zaremba <valtiel.zaremba@hauptstadtbytes.de>
2026-04-22 16:24:51 +0200

    BYTESNET-48: Updated gitignore to ignore BYTES.NET.Extensibility/bin

--- src-2023/BYTES.NET (Core)/BYTES.NET.Tests/IO/TestStringExtensions.cs
+++ src-2023/BYTES.NET (Core)/BYTES.NET.Tests/IO/TestStringExtensions.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
-using String = global::System.String;
 
 //import namespace(s) required from 'BYTES.NET' framework
 using BYTES.NET.IO;
@@ -16,22 +15,6 @@ namespace BYTES.NET.Tests.IO
     [TestClass]
     public class TestStringExtensions
     {
-        [TestMethod]
-        public void TestFilesAndFoldersExist()
-        {
-            string path = "%BYTES.NET.DIR%\\..\\..\\..\\..\\NotExisting";
-            Assert.AreEqual(false, path.DirectoryExists());
-
-            path = "%BYTES.NET.DIR%\\..\\..\\..\\..\\Sample Data";
-            Assert.AreEqual(true, path.DirectoryExists());
-
-            path = "%BYTES.NET.DIR%\\..\\..\\..\\..\\Sample Data\\DownloadedFile.pdf";
-            Assert.AreEqual(true, path.FileExists());
-
-            path = "%BYTES.NET.DIR%\\..\\..\\..\\..\\Sample Data\\NotExisting.pdf";
-            Assert.AreEqual(false, path.FileExists());
-
-        }
 
         [TestMethod]
         public void TestCheckForFilePath()
@@ -92,32 +75,32 @@ namespace BYTES.NET.Tests.IO
         {
             string inputPath = "%bytes.net.dir%";
             string[] resultArray = inputPath.ExpandWildcardPath();
-            Debug.WriteLine("Expanding variable(s) in '" + inputPath + "' resulted in '" + String.Join(",", resultArray) + "'");
+            Debug.WriteLine("Expanding variable(s) in '" + inputPath + "' resulted in '" + System.String.Join(",", resultArray) + "'");
             Assert.AreEqual(Framework.AssemblyDirectory, resultArray[0]);
 
             inputPath = "%bytes.net.dir%\\..\\..\\Debug\\net472\\*.dll";
             resultArray = inputPath.ExpandWildcardPath();
-            Debug.WriteLine("Expanding variable(s) in '" + inputPath + "' resulted in '" + String.Join(",", resultArray) + "'");
+            Debug.WriteLine("Expanding variable(s) in '" + inputPath + "' resulted in '" + System.String.Join(",", resultArray) + "'");
             Assert.AreEqual(10, resultArray.Length);
 
             inputPath = "%bytes.net.dir%\\..\\..\\Debug\\net472-NotExisting\\*.dll"; //test handling a non-existing path
             resultArray = inputPath.ExpandWildcardPath();
-            Debug.WriteLine("Expanding variable(s) in '" + inputPath + "' resulted in '" + String.Join(",", resultArray) + "'");
+            Debug.WriteLine("Expanding variable(s) in '" + inputPath + "' resulted in '" + System.String.Join(",", resultArray) + "'");
             Assert.AreEqual(0, resultArray.Length);
 
             inputPath = "%bytes.net.dir%\\..\\..\\Debug\\net*\\";
             resultArray = inputPath.ExpandWildcardPath();
-            Debug.WriteLine("Expanding variable(s) in '" + inputPath + "' resulted in '" + String.Join(",", resultArray) + "'");
+            Debug.WriteLine("Expanding variable(s) in '" + inputPath + "' resulted in '" + System.String.Join(",", resultArray) + "'");
             Assert.AreEqual(4, resultArray.Length);
 
             inputPath = "%bytes.net.dir%\\..\\..\\Debug\\n*72";
             resultArray = inputPath.ExpandWildcardPath();
-            Debug.WriteLine("Expanding variable(s) in '" + inputPath + "' resulted in '" + String.Join(",", resultArray) + "'");
+            Debug.WriteLine("Expanding variable(s) in '" + inputPath + "' resulted in '" + System.String.Join(",", resultArray) + "'");
             Assert.AreEqual(1, resultArray.Length);
 
             inputPath = "%bytes.net.dir%\\..\\..\\D*ug\\net*\\";
             resultArray = inputPath.ExpandWildcardPath();
-            Debug.WriteLine("Expanding variable(s) in '" + inputPath + "' resulted in '" + String.Join(",", resultArray) + "'");
+            Debug.WriteLine("Expanding variable(s) in '" + inputPath + "' resulted in '" + System.String.Join(",", resultArray) + "'");
             Assert.AreEqual(4, resultArray.Length);
 
             //check expansion for custom variables
@@ -126,7 +109,7 @@ namespace BYTES.NET.Tests.IO
 
             inputPath = "%demo.variable%\\..\\..\\Debug\\net472\\*.dll";
             resultArray = inputPath.ExpandWildcardPath(variables);
-            Debug.WriteLine("Expanding variable(s) in '" + inputPath + "' resulted in '" + String.Join(",", resultArray) + "'");
+            Debug.WriteLine("Expanding variable(s) in '" + inputPath + "' resulted in '" + System.String.Join(",", resultArray) + "'");
             Assert.AreEqual(10, resultArray.Length);
 
             string[] inputArray = new string[]
@@ -135,7 +118,7 @@ namespace BYTES.NET.Tests.IO
                 "%demo.variable%\\..\\..\\Debug\\net472\\*.dll"
             };
             resultArray = inputArray.ExpandWildcardPath(variables);
-            Debug.WriteLine("Expanding variable(s) in '" + String.Join(",", inputArray) + "' resulted in '" + String.Join(",", resultArray) + "'");
+            Debug.WriteLine("Expanding variable(s) in '" + System.String.Join(",", inputArray) + "' resulted in '" + System.String.Join(",", resultArray) + "'");
             Assert.AreEqual(10, resultArray.Length);
         }
     }
