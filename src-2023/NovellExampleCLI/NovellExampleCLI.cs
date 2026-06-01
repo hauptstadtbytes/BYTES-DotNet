82ccd4c4152cf5274b952ba34279c41449bd689a
Valtiel Zaremba <valtiel.zaremba@hauptstadtbytes.de>
2026-06-01 11:54:47 +0200

    BYTESNET-49: Added CLI project to showcase novell use

---
+++ src-2023/NovellExampleCLI/NovellExampleCLI.cs
@@ -0,0 +1,134 @@
+﻿using Novell.Directory.Ldap;
+using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class NovellExampleCLI
+{
+    static async Task Main()
+    {
+        Console.Write("Host: ");
+        string host = Console.ReadLine()!;
+
+        Console.Write("Username: ");
+        string username = Console.ReadLine()!;
+
+        Console.Write("Password: ");
+        string password = Console.ReadLine()!;
+
+        using var conn = await ConnectAsync(host);
+
+        bool authenticated = await AuthenticateAsync(conn, username, password);
+
+        string? baseDn = await GetBaseDnAsync(conn);
+        Console.WriteLine(baseDn);
+
+        await SearchWithFilter(conn, baseDn);
+
+        await PrintAllProperties(conn, baseDn);
+    }
+
+    //connect to service
+    static async Task<LdapConnection> ConnectAsync(string host)
+    {
+        var conn = new LdapConnection();
+        await conn.ConnectAsync(host, 636);
+
+        return conn;
+    }
+
+    //authenticate user
+    static async Task<bool> AuthenticateAsync(LdapConnection conn, string username, string password)
+    {
+        Console.WriteLine("---Authenticate---\n");
+        try
+        {
+            await conn.BindAsync(username, password);
+
+            Console.WriteLine($"Login successful for {username}");
+
+            return true;
+        }
+        catch (LdapException ex) when (ex.ResultCode == LdapException.InvalidCredentials)
+        {
+            Console.WriteLine("Invalid credentials");
+            return false;
+        }
+    }
+
+    static async Task<string?> GetBaseDnAsync(LdapConnection conn)
+    {
+        Console.WriteLine("---Domain---\n");
+
+        LdapEntry root = await conn.ReadAsync("",
+            new[]
+            {
+                "namingContexts",
+                "defaultNamingContext",
+                "*",
+                "+"
+            });
+
+        if (root.GetAttributeSet().ContainsKey("defaultNamingContext"))
+        {
+            return root.Get("defaultNamingContext").StringValue;
+        }
+
+        if (root.GetAttributeSet().ContainsKey("namingContexts"))
+        {
+            return root.Get("namingContexts").StringValue;
+        }
+
+        return null;
+    }
+
+    //Search with filter
+    static async Task SearchWithFilter(LdapConnection conn, string? baseDn)
+    {
+        if (string.IsNullOrWhiteSpace(baseDn))
+            return;
+
+        Console.WriteLine("---Search with filter (objectClass=person)---");
+
+        ILdapSearchResults results =
+            await conn.SearchAsync(baseDn, LdapConnection.ScopeSub, "(objectClass=person)", new[] { "mail" }, false);
+
+        await foreach (LdapEntry entry in results)
+        {
+            Console.WriteLine(entry.Dn);
+            Console.WriteLine("mail: " + entry.GetAttributeSet().GetAttribute("mail")?.StringValue);
+        }
+    }
+
+    //get and print all properties for entry
+    static async Task PrintAllProperties(LdapConnection conn, string? baseDn)
+    {
+        if (string.IsNullOrWhiteSpace(baseDn))
+            return;
+
+        Console.WriteLine("---List all properties---");
+
+        ILdapSearchResults results = await conn.SearchAsync(baseDn, LdapConnection.ScopeBase, "(objectClass=*)", null, false);
+
+        await foreach (LdapEntry entry in results)
+        {
+            PrintProperties(entry);
+            break;
+        }
+    }
+
+    //helper class to print properties
+    static void PrintProperties(LdapEntry e)
+    {
+        foreach (LdapAttribute a in e.GetAttributeSet())
+        {
+            foreach (string value in a.StringValueArray)
+            {
+                Console.WriteLine($"{a.Name}: {value}");
+            }
+        }
+    }
+}
