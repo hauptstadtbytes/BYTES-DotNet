c2cfda781328682890faee3e6f44da78817089cf
Valtiel Zaremba <valtiel.zaremba@hauptstadtbytes.de>
2026-06-10 09:34:10 +0200

    BYTESNET-30: Parted CLI in two CLI, added explicit typing, returned more than just filename

---
+++ src-2023/FtpCLI/FtpCLI.cs
@@ -0,0 +1,82 @@
+﻿// Use standard .Net amespaces
+using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Use Dependencies
+using BYTES.NET.IO;
+using FluentFTP;
+
+public class FtpCLI
+{
+    public static void Main()
+    {
+        FTP();
+    }
+
+    #region methods
+
+    /// <summary>
+    /// Builds and FTP connection and returns all files
+    /// </summary>
+    public static void FTP()
+    {
+        Console.Write("Host: ");
+        string host = Console.ReadLine()!;
+
+        Console.Write("Username: ");
+        string user = Console.ReadLine()!;
+
+        Console.Write("Password: ");
+        string pass = GetPasswordInput();
+
+        Console.WriteLine("\nConnecting...\n");
+
+        var client = new AsyncFtpClient(host, user, pass, 2121);
+
+        client.Connect().GetAwaiter().GetResult();
+
+        var items = client.GetListing("/").GetAwaiter().GetResult();
+
+        Console.WriteLine("Files:");
+
+        foreach (FtpListItem item in items)
+        {
+            Console.WriteLine($"{item.Name} ({item.Type}, {item.Size} bytes), Last modified: {item.Modified}");
+        }
+
+        client.Disconnect().GetAwaiter().GetResult();
+    }
+
+    #endregion
+
+
+    #region helper methods
+
+    /// <summary>
+    /// hide password input in terminal
+    /// </summary>
+    /// <returns></returns>
+    static string GetPasswordInput()
+    {
+        StringBuilder input = new StringBuilder();
+        Console.Write("Enter password....\n");
+        while (true)
+        {
+            var b = Console.ReadKey(true);
+
+            if (b.Key == ConsoleKey.Enter)
+                break;
+            if (b.Key == ConsoleKey.Backspace && input.Length > 0)
+                input.Remove(input.Length - 1, 1);
+            else if (b.Key != ConsoleKey.Backspace)
+                input.Append(b.KeyChar);
+        }
+        return input.ToString();
+    }
+
+    #endregion
+}
---
+++ src-2023/SFTP+FTP CLI/SftpCLI.cs
@@ -0,0 +1,87 @@
+﻿// Use standard .Net amespaces
+using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Use Dependencies
+using BYTES.NET.IO;
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+
+public class SftpCLI
+{
+    public static void Main()
+    {
+        SFTP();
+    }
+
+
+    #region private methods
+
+    /// <summary>
+    /// builds and SFTP connection and returns all files
+    /// </summary>
+    private static void SFTP()
+    {
+        Console.Write("Host: ");
+        string host = Console.ReadLine()!;
+
+        Console.Write("Username: ");
+        string user = Console.ReadLine()!;
+
+        Console.Write("Password: ");
+        string pass = GetPasswordInput();
+
+        Console.WriteLine("\nConnecting...\n");
+
+        using SftpClient client = new SftpClient(host, 2222, user, pass);
+        client.Connect();
+
+        var files = client.ListDirectory("/");
+
+        Console.WriteLine("Files:");
+
+        foreach (ISftpFile file in files)
+        {
+            if (file.Name == "." || file.Name == "..")
+                continue;
+
+            Console.WriteLine($"{file.Name} ({file.Length} bytes), Last modified: {file.LastWriteTime}");
+        }
+
+        client.Disconnect();
+    }
+
+    #endregion
+
+
+    #region helper methods
+
+    /// <summary>
+    /// hide password input in terminal
+    /// </summary>
+    /// <returns></returns>
+    static string GetPasswordInput()
+    {
+        StringBuilder input = new StringBuilder();
+        Console.Write("Enter password....\n");
+        while (true)
+        {
+            var b = Console.ReadKey(true);
+
+            if (b.Key == ConsoleKey.Enter)
+                break;
+            if (b.Key == ConsoleKey.Backspace && input.Length > 0)
+                input.Remove(input.Length - 1, 1);
+            else if (b.Key != ConsoleKey.Backspace)
+                input.Append(b.KeyChar);
+        }
+        return input.ToString();
+    }
+
+    #endregion
+
+}
